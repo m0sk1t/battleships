91ed19b62436164747c3deab69a615c1e7a51504
Artem Litvinov <artem.litvinov@rp.ru>
2017-05-05 16:01:44 +0300

    random ships generation

---
+++ src/CreateShips.js
@@ -0,0 +1,116 @@
+const CreateShips = (shipsSizeArr) => {
+  let ships = [],
+    map = new Array(10);
+
+  for (let i = 0; i < map.length; i++) {
+    map[i] = new Array(10);
+  }
+  for (let i = 0; i < map.length; i++) {
+    for (let j = 0; j < 10; j++) {
+      map[i][j] = -1;
+    }
+  }
+
+  createShip:
+    for (let i = 0; i < shipsSizeArr.length; i++) {
+      let currSize = shipsSizeArr[i],
+        ship = {
+          'size': currSize,
+          'classId': i,
+          'layout': []
+        }
+      let anchorPoint = CreateAnchorPoint(ship.size),
+        y = anchorPoint[0],
+        x = anchorPoint[1],
+        directionChanged = false;
+
+      if (map[y][x] != -1) {
+        i--;
+        continue createShip;
+      }
+
+      let direction = RandDirection(),
+        tempLayout = [];
+
+      if (direction === 'x') {
+        let currX = x;
+
+        for (let j = 1; j < ship.size; j++) {
+          currX++;
+
+          if (map[y][currX] != -1) {
+            if (!directionChanged) {
+              i--;
+              continue createShip;
+            }
+            i--;
+            continue createShip;
+          }
+
+          tempLayout.push([y, currX]);
+        }
+      } else {
+        let currY = y;
+
+        for (let j = 1; j < ship.size; j++) {
+          currY++;
+
+          if (map[currY][x] != -1) {
+            if (!directionChanged) {
+              i--;
+              continue createShip;
+            }
+            i--;
+            continue createShip;
+          }
+
+          tempLayout.push([currY, x]);
+        }
+      }
+
+      ship.layout = [anchorPoint, ...tempLayout];
+      ship.direction = direction;
+      ships.push(ship);
+      let updatedMap = updMap(map, ship);
+      map = updatedMap;
+    }
+  return ships;
+}
+
+function CreateAnchorPoint(shipSize) {
+  return [RandomInteger(0, 10 - shipSize), RandomInteger(0, 10 - shipSize)]
+}
+
+function RandomInteger(min, max) {
+  var rand = min + Math.random() * (max + 1 - min);
+  rand = Math.floor(rand);
+  return rand;
+}
+
+function RandDirection() {
+  return (Math.random() - 0.5) > 0 ? 'x' : 'y';
+}
+
+function updMap(map, ship) {
+  ship.layout.forEach(part => {
+    let y = part[0],
+      x = part[1],
+      doSomth = 0;
+
+    map[y][x] = ship.size;
+
+    map[y - 1] != null ? map[y - 1][x - 1] != null ? map[y - 1][x - 1] = 0 : 0 : 0;
+    map[y - 1] != null ? map[y - 1][x    ] != null ? map[y - 1][x    ] = 0 : 0 : 0;
+    map[y - 1] != null ? map[y - 1][x + 1] != null ? map[y - 1][x + 1] = 0 : 0 : 0;
+    map[y    ] != null ? map[y    ][x - 1] != null ? map[y    ][x - 1] = 0 : 0 : 0;
+    map[y    ] != null ? map[y    ][x + 1] != null ? map[y    ][x + 1] = 0 : 0 : 0;
+    map[y + 1] != null ? map[y + 1][x - 1] != null ? map[y + 1][x - 1] = 0 : 0 : 0;
+    map[y + 1] != null ? map[y + 1][x    ] != null ? map[y + 1][x    ] = 0 : 0 : 0;
+    map[y + 1] != null ? map[y + 1][x + 1] != null ? map[y + 1][x + 1] = 0 : 0 : 0;
+
+  })
+
+  return map;
+}
+
+export default CreateShips;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import gameInfo from './reducers'
 import Game from './components/Game'
+import CreateShips from './createShips'
 import { addShips, createMap } from './actions'
 
 let store = createStore(gameInfo);
@@ -14,13 +15,11 @@ store.subscribe(() =>
   console.log(store.getState())
 )
 
-store.dispatch(addShips('p1', [ {size: 5, layout: [[0,1], [1,1], [2,1], [3,1], [4,1]]} ]));
-store.dispatch(addShips('p1', [ {size: 4, layout: [[6,2], [6,3], [6,4], [6,5]]}, {size: 3, layout: [[9,7], [8,7], [7,7]]}, {size: 3, layout: [[2,5], [2,6], [2,7]]}, {size: 2, layout: [[4,9], [5,9]]} ]));
+store.dispatch(addShips( 'p1', CreateShips([5, 4, 3, 3, 2]) ));
 store.dispatch(createMap('p1', store.getState().ships.p1));
 
-store.dispatch(addShips('p2', [ {size: 5, layout: [[0,1], [1,1], [2,1], [3,1], [4,1]]} ]));
-store.dispatch(addShips('p2', [ {size: 4, layout: [[6,2], [6,3], [6,4], [6,5]]}, {size: 3, layout: [[9,7], [8,7], [7,7]]}, {size: 3, layout: [[2,5], [2,6], [2,7]]}, {size: 2, layout: [[4,9], [5,9]]} ]));
-store.dispatch(createMap('p2', store.getState().ships.p1));
+store.dispatch(addShips( 'p2', CreateShips([5, 4, 3, 3, 2]) ))
+store.dispatch(createMap('p2', store.getState().ships.p2));
 
 render(
   <Provider store={store}>
